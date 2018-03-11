37db68116437103b209864c5814b132f0f8077aa
Eblank87 <34084348+Eblank87@users.noreply.github.com>
2018-03-10 20:34:25 -0800

    added test

--- HW_Votes.py
+++ HW_Votes.py
@@ -2,12 +2,38 @@
 import os
 import csv
 
-#set paths for files
-csvpath1 = os.path.join("PythonHW","Python-Challenge","election_data_1.csv")
-csvpath2 = os.path.join("PythonHW","Python-Challenge","election_data_2.csv")
 
+
+#loop through data sets
+for sets in datasets:
+    #grab polling CSVs
+    votecsv = os.path.join("PythonHW","Python-Challenge","election_data_" + sets + ".csv")
+    
+    #create new CSV
+    new_vote_data_csv= os.path.join("PythonHW","Python-Challenge","election_data_" + sets + ".csv")
+    #set new columns
+    Voter_ID[]
+    County[]
+    Candidate[]
+    
 #open each CSV
-with open(csvpath1, newline="" as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-with open(csvpath2, newline="" as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")    
+    with open(csvpath, encoding="utf-8" as csvfile:
+       csvreader = csv.reader(csvfile, delimiter=",")
+       
+       #skip headers
+       next(csvreader, None)
+       for row in  
+
+Candidate_List =[]
+Candidate_Votes= 
+Winner = 
+
+#show results
+print("Election Results")
+print("--------------------")
+print("Total Votes: " + str(int(total_votes))
+print("--------------------")
+#list of candidates with votes/%
+print("--------------------")
+print("Winner: "+ str(winner))
+print("--------------------")
