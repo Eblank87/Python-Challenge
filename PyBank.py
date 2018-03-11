82f7d7a513f9bb91ca736be74a94e57873f5c188
Eblank87 <34084348+Eblank87@users.noreply.github.com>
2018-03-11 12:20:17 -0700

    updates 3/11_2

---
+++ PyBank.py
@@ -0,0 +1,17 @@
+import os
+import csv
+
+budgetdata = os.path.join("LearnPython","HW-Python","PyBank","budget_data_1.csv")
+
+#create final variables to print 
+total_months = 
+total_revenue=
+avg_rev_change =
+max_increase = 
+max_decrease = 
+
+print("Financial Analysis")
+print("--------------------")
+print("Average Revenue Change: " + int(avg_rev_change))
+print("Greatest Increase in Revenue: " + )
+print("Greatest Decrease in Revenue: " + )
--- PyBoss.py
+++ PyBoss.py
@@ -28,57 +28,11 @@ csvfile:
         SSN.append(row[3])
         State.append(row[4])
 
-   us_state_abbrev = { 'Alabama': 'AL',
-    'Alaska': 'AK',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY',
-}
+   First_Name = Name.split()
+   Last_Name = Name.split()
+   DOB_New = DOB.split()
+   SSN_New = SSN.split()
+   State_abr = {'Alabama': 'AL','Alaska': 'AK','Arizona': 'AZ','Arkansas': 'AR', 'California': 'CA','Colorado': 'CO','Connecticut': 'CT','Delaware': 'DE','Florida': 'FL','Georgia': 'GA','Hawaii': 'HI','Idaho': 'ID', 'Illinois': 'IL','Indiana': 'IN','Iowa': 'IA','Kansas': 'KS', 'Kentucky': 'KY','Louisiana': 'LA','Maine': 'ME','Maryland': 'MD','Massachusetts': 'MA','Michigan': 'MI','Minnesota': 'MN','Mississippi': 'MS','Missouri': 'MO','Montana': 'MT', 'Nebraska': 'NE','Nevada': 'NV','New Hampshire': 'NH','New Jersey': 'NJ','New Mexico': 'NM','New York': 'NY','North Carolina': 'NC','North Dakota': 'ND','Ohio': 'OH','Oklahoma': 'OK','Oregon': 'OR','Pennsylvania': 'PA','Rhode Island': 'RI','South Carolina': 'SC','South Dakota': 'SD','Tennessee': 'TN','Texas': 'TX','Utah': 'UT','Vermont': 'VT', 'Virginia': 'VA','Washington': 'WA','West Virginia': 'WV','Wisconsin': 'WI', 'Wyoming': 'WY',}
 
 #zip lists together
     cleancsv = zip(Emp_ID, First_Name, Last_Name, DOB, SSN, State)
@@ -87,7 +41,7 @@ csvfile:
         csvwriter = csv.writer(csvFile, delimiter=',')
     
     #write headers into file
-    csvwriter.writerrow(["Emp_ID","First_Name","Last_Name", "DOB", "SSN", "State")
+    csvwriter.writerrow(["Emp_ID","First_Name","Last_Name", "DOB_New", "SSN_New", "State_abr")
     ])
         #write the zipped list into a CSV
         csvwriter.writerows(cleancsv)
