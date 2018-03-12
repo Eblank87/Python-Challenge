da26f2a2b10484d1bd2d3fed184a0ae087955905
Eblank87 <34084348+Eblank87@users.noreply.github.com>
2018-03-11 22:44:38 -0700

    final_1

--- PyBoss.py
+++ PyBoss.py
@@ -36,7 +36,7 @@ DOB_New = str(Month)+"/"+str(Day)+"/"+str(Year)
 
 SSN_New = str("***-**-"+(int(SSN.split("-",[2])))
 #convert State Name into State Abreviation by looking up abr in dictionary
- 
+
 state_final = [] 
     State_abr = {'Alabama': 'AL','Alaska': 'AK','Arizona': 'AZ','Arkansas': 'AR', 'California': 'CA','Colorado': 'CO','Connecticut': 'CT','Delaware': 'DE','Florida': 'FL','Georgia': 'GA','Hawaii': 'HI','Idaho': 'ID', 'Illinois': 'IL','Indiana': 'IN','Iowa': 'IA','Kansas': 'KS', 'Kentucky': 'KY','Louisiana': 'LA','Maine': 'ME','Maryland': 'MD','Massachusetts': 'MA','Michigan': 'MI','Minnesota': 'MN','Mississippi': 'MS','Missouri': 'MO','Montana': 'MT', 'Nebraska': 'NE','Nevada': 'NV','New Hampshire': 'NH','New Jersey': 'NJ','New Mexico': 'NM','New York': 'NY','North Carolina': 'NC','North Dakota': 'ND','Ohio': 'OH','Oklahoma': 'OK','Oregon': 'OR','Pennsylvania': 'PA','Rhode Island': 'RI','South Carolina': 'SC','South Dakota': 'SD','Tennessee': 'TN','Texas': 'TX','Utah': 'UT','Vermont': 'VT', 'Virginia': 'VA','Washington': 'WA','West Virginia': 'WV','Wisconsin': 'WI', 'Wyoming': 'WY',}
 
@@ -48,8 +48,8 @@ print(state_final)
 
 #zip lists together
     cleancsv = zip(Emp_ID, First_Name, Last_Name, DOB_New, SSN, State_Final)
-      
-    
+   
+
 newemployeecsv = os.path.join('newemployee_data_1.csv')              
 with open('newemployee_data_1.csv',"w",newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
